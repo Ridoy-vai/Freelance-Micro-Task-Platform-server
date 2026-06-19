29b2496c9b9159d9ce830db418b43453d4f41e6b
Ridoy-vai <mdsahariyarridoy@gmail.com>
2026-06-19 08:04:02 +0600

    server creat succesfull

---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+const cors = require('cors');
+require('dotenv').config();
+app.use(cors());
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = process.env.MONGODB_URI;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        const connection = await client.connect();
+        const db = connection.db("Freelance-Microtask-Platform");
+        const TasksCollection = db.collection("Tasks");
+        const FreelancersCollection = db.collection("Freelancers");
+        const ClientsCollection = db.collection("Clients");
+        const ReviewsCollection = db.collection("Reviews");
+
+
+        // task related funtion
+        app.post('/tasks', async (req, res) => {
+            const task = req.body;
+            const result = await TasksCollection.insertOne(task);
+            res.send(result);
+        });
+        app.get('/tasks', async (req, res) => {
+            const limit = parseInt(req.query.limit) || 0; // Get the limit from query parameters, default to 0 (no limit)
+            const result = await TasksCollection.find().limit(limit).toArray();
+            res.send(result);
+        });
+        app.get('/tasksid/:id', async (req, res) => {
+            const id = req.params.id;
+            const result = await TasksCollection.findOne({  _id: new ObjectId(id) });
+            res.send(result);
+        });
+        app.get("/my-tasks/:clientId", async (req, res) => {
+            const clientId = req.params.clientId;
+            const result = await TasksCollection.find({
+                ClientId: clientId,
+            }).toArray();
+            res.send(result);
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
